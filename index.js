0f2369a25d0f3d1560c7f8b9dccfa01c32b34dc2
samanthamarberger <slatham04@gmail.com>
2022-09-24 20:20:19 -0600

    commit tree 563f92603c51c59ac482a3cbc59d2b3ba5e73b2e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let element = record.find(isWin);
+    if (element != undefined){
+        return element.year;
+    }
+}
+
+function isWin(element, index, array) {
+    return (element.result === "W");  
+}
+
+
+// year
+// result
